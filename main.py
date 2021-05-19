95a910057aba37fde5ed872e3fd79668a3d0f24b
abhinand0d <abhinand0d@gmail.com>
2021-05-19 19:46:10 +0530

    Changed Filename

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import youtube_dl as yd
-
+import time
 import os
 
 url = 'https://yt-downloader-by-abhi.herokuapp.com/Music'
 if os.path.exists('Music'):
     print("Path Exist")
+    os.path.basename()
 else:
     os.mkdir('Music')
     print("Created the path")
@@ -19,11 +20,12 @@ def download_to_client():
     global get_title
     st.markdown('### Download Mp3 ###')
     # st.markdown('<a href="'+url+'/'+get_title+'.mp3" download>Download'+get_title+'</a>',unsafe_allow_html=True )
-    st.audio("Music/"+get_title+".mp3")
+    st.audio("Music/Music"+str(timea)+".mp3")
 
 
 def download():
-    global get_title
+    global get_title,timea
+    timea = time.time()
     ymdl_opts = {}
 
     with yd.YoutubeDL(ymdl_opts) as yddl:
@@ -31,7 +33,7 @@ def download():
         get_title = meta['title']
 
     ydl_ops = {
-    'outtmpl':'Music/%(title)s.%(ext)s',
+    'outtmpl':'Music/Music'+str(timea)+'.%(ext)s',
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
@@ -43,6 +45,7 @@ def download():
     def download_vid():
         with yd.YoutubeDL(ydl_ops) as ydl:
             ydl.download([zxt])
+            
 
     zxt = input.strip()
     download_vid()
